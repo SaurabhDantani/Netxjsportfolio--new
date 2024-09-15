98695b4cff787ae37992f1e944b6959f2bc7c152
Saurabh Dantani <62558826+SaurabhDantani@users.noreply.github.com>
2024-09-15 12:29:14 +0000

    updated code

--- app/page.tsx
+++ app/page.tsx
@@ -10,68 +10,11 @@ import {
   IconNewSection,
   IconTerminal2,
  } from '@tabler/icons-react';
+ 
 import { FloatingDock } from '@/components/ui/floating-dock';
+import { Button } from '@/components/ui/button';
 export default function Home() {
   const [mounted, setMounted] = useState(false);
-
-  const links = [
-    {
-      title: "Home",
-      icon: (
-        <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "#",
-    },
-
-    {
-      title: "Products",
-      icon: (
-        <IconTerminal2 className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "#",
-    },
-    {
-      title: "Components",
-      icon: (
-        <IconNewSection className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "#",
-    },
-    // {
-    //   title: "Aceternity UI",
-    //   icon: (
-    //     <Image
-    //       src="https://assets.aceternity.com/logo-dark.png"
-    //       width={20}
-    //       height={20}
-    //       alt="Aceternity Logo"
-    //     />
-    //   ),
-    //   href: "#",
-    // },
-    {
-      title: "Changelog",
-      icon: (
-        <IconExchange className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "#",
-    },
-
-    {
-      title: "Twitter",
-      icon: (
-        <IconBrandX className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "#",
-    },
-    {
-      title: "GitHub",
-      icon: (
-        <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "#",
-    },
-  ];
 
   useEffect(() => {
     setMounted(true);
@@ -81,8 +24,8 @@ export default function Home() {
 
   return (
     <>
+    <div style={{marginTop: '500px'}}>
       <BackgroundBeamsWithCollision>
-        <div >
           <h2 className="text-2xl relative z-20 md:text-4xl lg:text-7xl font-bold text-center text-black dark:text-white font-sans tracking-tight">
             What&apos;s cooler than Beams?{" "}
             <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
@@ -95,8 +38,10 @@ export default function Home() {
             </div>
           </h2>
 
-        </div>
+        <Button>Button</Button>
       </BackgroundBeamsWithCollision>
+    </div>
     </>
   );
 }
+
